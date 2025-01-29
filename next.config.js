da2dfd384f9d7551527807a310e1b8ee8540cadb
Fareny <pd.fareny@gmail.com>
2025-01-29 17:12:38 +0300

    Initial commit

--- next.config.js
+++ next.config.js
@@ -1,7 +1,8 @@
 const withPWA = require('next-pwa')({
-  dest: '.next',
+  dest: 'public',
   register: true,
   skipWaiting: true,
+  disable: process.env.NODE_ENV !== 'production',
 });
 
 module.exports = withPWA({
